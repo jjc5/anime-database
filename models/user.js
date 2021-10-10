94b902a4595b6194892ef34b944a5bacfd005afb
Jason C <jayingmantis.555@gmail.com>
2021-10-10 18:51:06 -0500

    add user model

---
+++ models/user.js
@@ -0,0 +1,9 @@
+const { Schema, model } = require('mongoose');
+
+const userSchema = new Schema({
+  email: {type: String, unique: true, required: true},
+  password: {type: String, required: true},
+  favorites: [{type: Schema.Types.ObjectId, ref: "Favorite"}]
+}, {
+  timestamps: true;
+});
